ec9a270926a5ada6dd57207d66a79eddae966a56
beef <beeflawg@gmail.com>
2018-11-08 14:00:48 -0700

    files

---
+++ app/data/friends.js
@@ -0,0 +1,54 @@
+var friendsArr = [
+    {
+      name: "Ahmed",
+      photo: "ahmed@example.com",
+      scores: [
+        "1",
+        "2",
+        "3",
+        "1",
+        "2",
+        "3",
+        "1",
+        "2",
+        "3",
+        "1"
+      ]
+    },
+    {
+      name: "beth",
+      photo: "ahmed@example.com",
+      scores: [
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+      ]
+    },
+    {
+      name: "beef",
+      photo: "ahmed@example.com",
+      scores: [
+        "4",
+        "2",
+        "3",
+        "4",
+        "2",
+        "4",
+        "1",
+        "4",
+        "3",
+        "1"
+      ]
+    }
+  ];
+
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendsArr;
